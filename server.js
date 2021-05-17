5d09d7046a6e339aca81d8cd9f7e98753393917c
ericpkatz <ericpkatz@gmail.com>
2021-05-17 19:25:52 -0400

    added main route with express

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
-const db = require('./db');
+const express = require('express');
+const app = express();
+
+const { syncAndSeed, models: { Sneaker } } = require('./db');
+
+
+app.get('/', (req, res)=> res.redirect('/sneakers'));
+
+app.get('/sneakers', async(req, res, next)=> {
+  try {
+    const sneakers = await Sneaker.findAll();
+    const mapped = sneakers.reduce((acc, sneaker)=> {
+      const key = sneaker.brand;
+      acc[key] = acc[key] || [];
+      acc[key].push(sneaker);
+      return acc;
+    }, {});
+    res.send(`
+      <html>
+        <head>
+          <title>Sneakers</title>
+        </head>
+        <body>
+          <h1>Sneaker World (${ sneakers.length })</h1>
+          <ul>
+            ${
+              Object.entries(mapped).map( entry => {
+                const brand = entry[0];
+                const sneakers = entry[1];
+                return `
+                  <li>
+                    <a href='/sneakers/${brand}'>${ brand } (${sneakers.length})</a>
+                  </li>
+                `;
+              }).join('')
+            }
+          </ul>
+        </body>
+      </html>
+    `);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 
 const run = async()=> {
   try {
-    await db.syncAndSeed();
+    await syncAndSeed();
     console.log('synced and seeded');
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> console.log(`listening on port ${port}`));
   }
   catch(ex){
     console.log(ex);
